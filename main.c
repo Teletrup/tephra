02173043cd9d5cd2eed6113efa4ab8b2199ca3d0
Teletrup <saletrapotasowatosyf@gmail.com>
2020-09-16 08:06:06 +0200

    dj glks óóó egihjegrgntmred jjj dgij nvnvnvnvnvnvp

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include "colors.h"
 
 #define MEM_SZ 512
 #define IN_BUF_SZ 64
 
-#define HEAD mem[0]
-#define IN_BUF mem + MEM_SZ - IN_BUF_SZ
+#define PC_ADDR 0
+#define PC_VAL ((short*) mem + PC_ADDR)[0]
+#define IN_BUF MEM_SZ - IN_BUF_SZ
 
 #define MIN(a, b) (a) < (b) ? (a) : (b)
 
@@ -13,17 +15,30 @@ char in_buf[64];
 unsigned char mem[MEM_SZ];
 
 
-void xxd(unsigned char* base, int len, int offset) {
+char* getByteForm(int addr, char val) {
+	if (addr == PC_ADDR || addr == PC_ADDR + 1)
+		return FG_RED BG_BLACK BYTE_FORM COLOR_RESET;
+	if (addr == PC_VAL || addr == PC_VAL + 1)
+		return FG_BLACK BG_RED BYTE_FORM COLOR_RESET;
+	if (addr >= IN_BUF)
+		return FG_MAGENTA BG_BLACK BYTE_FORM COLOR_RESET;
+	//TODO coloring instructions
+	return BYTE_FORM;
+	//Should it output reset after every byte?
+	//Should I even benchmark it?
+}
+
+void xxd(unsigned char* base, int len, int offset, char* (*ffun) (int, char)) {
 	char ascii_buf[17];
-	int max_addr = len + offset;
-	for (int i=offset; i<max_addr; i+=16) {
+	int limit = len + offset;
+	for (int i=offset; i<limit; i+=16) {
 		printf("%08X: ", i);
-		int line_len = MIN(16, max_addr - i); //ob1?
+		int line_len = MIN(16, limit - i);
 		ascii_buf[line_len] = 0;
 		int j;
 		for (j=0; j<line_len; j++) {
 			unsigned char chr = base[i + j];
-			printf("%02X", chr);
+			printf(ffun(i + j, chr), chr);
 			if (j % 2)
 				putchar(' ');
 			if (chr >= ' ' && chr <= '~')
@@ -31,26 +46,23 @@ void xxd(unsigned char* base, int len, int offset) {
 			else
 				ascii_buf[j] = '.';
 		}
-		int padding_len = (16 - line_len) * 2 + (16 - line_len + 1) / 2 + 1; //+1?
-		char padding_buf[39];
+		int padding_len = (16 - line_len) * 2 + (16 - line_len + 1) / 2 + 1;
+		char padding_buf[40];
 		padding_buf[padding_len] = 0;
 		for (int i=0; i<padding_len; i++)
 			padding_buf[i] = ' ';
-		printf(padding_buf);
-		printf(ascii_buf);
+		printf("%s%s", ascii_buf, padding_buf);
 		putchar('\n');
 	}
 }
 
 int main() {
-#ifdef DBG
-	for (int i=0; i<MEM_SZ; i++) {
-		mem[i] = 'A';
-	}
-#endif
-	HEAD = 128;
+	PC_VAL = 257;
 	while (1) {
-		xxd(mem, MEM_SZ, 0);
-		fgets(IN_BUF, IN_BUF_SZ, stdin);
+		printf(CLEAR);
+		xxd(mem, MEM_SZ, 0, getByteForm);
+		printf("\n");
+		fgets(mem + IN_BUF, IN_BUF_SZ, stdin);
+		PC_VAL++;
 	}
 }
